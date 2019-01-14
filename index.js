6ea85c487d79165df3e9417318037375c787ae00
Your Name <you@example.com>
2019-01-14 05:37:06 -0500

    first draft

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-   //$(".slide").hide();
-   //$("#s1").show();
+   var patch1 = 0; var patch2 = 0; var patch3 = "#s1"; var patch4 = "#s";
+   $(".slide").hide();
+   $("#s1").show();
+   $(".switch span").click(function() {
+      patch2 = $(this).attr("class");
+      $(".slide").hide();
+      patch3 = patch4.concat(patch2);
+      $(patch3).show();
+      patch1 = parseInt(patch2, 10) - 1; 
+   });
+   function clicked1(patchVar) {
+      console.log(patchVar);
+      $('html,body').animate({
+            scrollTop: $(patchVar).offset().top},
+      'slow');
+   }
+   $("#sec").click(function() {
+      clicked1(patch3);
+   });
 });
